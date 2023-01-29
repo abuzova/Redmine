d484ba93be5ae1bd92abe4281cb26e55b94d4d59
abuzova <alla.abuzova@gmail.com>
2023-01-29 22:32:29 +0100

    cleaned the code from unnecessary comments

--- tests/helper/constants.js
+++ tests/helper/constants.js
@@ -1,12 +1,6 @@
-// constants
+exports.Constants = class Constants {
+  
+    SEARCH_WORD = 'activity';
 
-const {expect} = require('@playwright/test')
-exports.Constants = class Constants{
-    
-    constructor() {
-        this.SEARCH_WORD = 'activity';
-        this.SEARCH_WORD_EX = "'" + this.SEARCH_WORD + "'";
-        this.SEARCH_WORD_EXE = /this.SEARCH_WORD/;
-    }     
 }
 
--- tests/specs/test.spec.js
+++ tests/specs/test.spec.js
@@ -1,82 +1,33 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const { HeaderPage } = require('../pages/header-page.js');
-const { SearchPage } = require('../pages/search-page.js');
-
-//const { Constants } = require('../helper/constants.js');
-
-test.skip('Main menu', async ({ page }) => {
-  const header = new HeaderPage(page);
-  await header.goto();
-
-  await header.isOverviewPage();
-  await header.isDownloadPage();
-  await header.isActivityPage();
-  await header.isRoadmapPage();
-  await header.isIssuesPage();
-  await header.isNewsPage();
-  await header.isWikiPage();
-  await header.isForumsPage();
-  await header.isRepositoryPage();
-});
-
-test.skip('Top menu', async ({ page }) => {
-  const header = new HeaderPage(page);
-  await header.goto();
-
-  await header.isProjectsPage();
-  await header.isHelpPage();
-  await header.isHomePage();
-});
+const { Constants } = require('../helper/constants.js');
 
 test('Searching', async ({ page }) => {
   const header = new HeaderPage(page);
-  const search = new SearchPage(page);
-  //const constants = new Constants();
+  const constants = new Constants();
   
   await header.goto();  
 
-  //await header.searchField.type(constants.SEARCH_WORD); // need to create file with constants 
-  //await page.keyboard.press('Enter');
-  //await search.haveTextSearchResultTitle();
-  //await search.haveTextSearchResultDescription();
-  //await expect(search.searchResultDescription).toHaveText(/activity/);
-  
-  //console.log(constants.SEARCH_WORD);
-
-  // 1) Results (70) should be equal checked pages
-  // 2) Each result should have title and/or description with searchin word or phrase
-
-  await header.searchField.type('activity');
+  await header.searchField.type(constants.SEARCH_WORD);
   await page.keyboard.press('Enter');
-  const re = new RegExp('activity');
+  const searchWordRegExp = new RegExp(constants.SEARCH_WORD);
 
   let zeroOrOneError = true;
   for (let i = 1; i < 21; i += 2) {
-    const previousErrorsCount = test.info().errors.length;
+    const previousErrorsCount = test.info().errors.length; // 
 
     let searchResultTitle = page.locator('dl#search-results :nth-child('+ i +').wiki-page'); 
     let searchResultDescription = page.locator('dl#search-results :nth-child('+ (i + 1) +') span.description');   
-    await expect.soft(searchResultTitle).toHaveText(re, {ignoreCase:true} );
-    await expect.soft(searchResultDescription).toHaveText(re, {ignoreCase:true} );
-    console.log("running i = ", i);
-    console.log("running i = ", (i + 1));        
-
-    console.log("test errors ", test.info().errors.length);  
-    const newErrorsCount = test.info().errors.length - previousErrorsCount; // 0, 1, 2
-
-    console.log("new errors!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ", newErrorsCount);  
+    await expect.soft(searchResultTitle).toHaveText(searchWordRegExp, {ignoreCase:true} );
+    await expect.soft(searchResultDescription).toHaveText(searchWordRegExp, {ignoreCase:true} );
+    
+    // 
+    const newErrorsCount = test.info().errors.length - previousErrorsCount; // 0, 1, 2    
 
     if (newErrorsCount > 1) {
       zeroOrOneError = false;
     }
-    //expect(newErrorsCount).toBeLessThanOrEqual(1); 
   }
-  test.fail(zeroOrOneError, 'oh no, failing because more than one error');
-  //expect(zeroOrOneError).toBeTruthy(); 
-
-  //await expect.soft(page.getByTestId('status')).toHaveText('Success');  
+  test.fail(zeroOrOneError, 'Failing because more than one error');  
 });
-
-
-
